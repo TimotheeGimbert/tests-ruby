ed807c58bd45725aac6f8c336ec23de327935301
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-11 22:49:11 +0200

    improvements

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,14 +1,50 @@
 def translate(str)
-  str
-      .split # [str] each word separated by space put into new array
-      .map {|word| 
-        if word[0].casecmp("aeiouy") == -1 
-          word+="ay" 
-        elsif word[0].casecmp("aeiouy") == 1 && word[1].casecmp("aeiouy") == 1
-          word = word.split('').rotate(2).join + "ay"
-        elsif word[0].casecmp("aeiouy") == 1
-          word = word.reverse + "ay"
-        end
-        }
+  if get_nb_words(str) == 1 
+    translate_word(str)
+  else
+    str
+      .split 
+      .map {|word| word = translate_word(word)}
       .join(' ')
+  end
 end
+
+def translate_word(word) # applies the rules to a single word
+  if is_voyel?(word[0])
+    add_ay(word)
+  else
+    cons = count_consonants(word)
+    add_ay(rotate_word(word, cons))
+  end
+end
+
+def count_consonants(word)
+  if !is_voyel?(word[0]) && !is_voyel?(word[1]) && !is_voyel?(word[2])
+    return 3
+  elsif !is_voyel?(word[0]) && !is_voyel?(word[1])
+    return 2
+  elsif !is_voyel?(word[0])
+    return 1
+  end
+end
+
+def is_voyel?(char) # takes a character and returns true if it's a voyel, false if it's a consonant
+  "aeiouy".include?(char)
+end
+
+def get_nb_words(str)
+  str.split.length
+end
+
+def add_ay(word)
+  word += "ay"
+end
+
+def rotate_word(word, n=1)
+  word
+      .split('')
+      .rotate(n)
+      .join
+end
+
+
