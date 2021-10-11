ac86f0c9773295abeeb723fcfbb52a6e50c39f79
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-12 00:13:32 +0200

    commenting exo4

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -12,15 +12,21 @@ end
 
 def start_of_word(str, n)
   str
-      .split('')
-      .reject {|c| str.index(c) >= n}
-      .join
+      .split('') # splits the string into array of chars
+      .reject {|c| str.index(c) >= n} # takes off the array all chars from the given position n
+      .join # joins back the array as a concatenated string
 end
 
 def first_word(str)
-  str.split.shift
+  str
+    .split # splits the given string into an array of words
+    .shift # returns the first element of the array
 end
 
 def titleize(str)
-  str.capitalize.split.map{|w| w.length >3 ? w.capitalize : w}.join(' ')
+  str
+    .capitalize # assures a capital at the beginning of the sentence
+    .split # splits the string into an array of words
+    .map {|w| w.length >3 ? w.capitalize : w} # capitalizes words greater than 3 chars
+    .join(' ') # joins back the modified array as a space-separated string
 end
